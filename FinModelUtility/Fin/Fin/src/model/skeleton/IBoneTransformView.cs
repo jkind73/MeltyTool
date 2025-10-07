3a09e2e12a45e3fc9e8d48334cf1a44a92d26e0e
MeltyPlayer <meltyplayer@gmail.com>
2025-10-06 20:29:48 -0500

    Started setting up better bone transform logic to simplify things downstream.

---
+++ FinModelUtility/Fin/Fin/src/model/skeleton/IBoneTransformView.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace fin.model.skeleton;
+
+public interface IBoneTransformView {
+  bool TryGetWorldTranslation(IReadOnlyBone bone, out Vector3 position);
+  bool TryGetWorldRotation(IReadOnlyBone bone, out Quaternion rotation);
+  bool TryGetWorldScale(IReadOnlyBone bone, out Vector3 scale);
+
+  bool TryGetLocalTranslation(IReadOnlyBone bone, out Vector3 position);
+  bool TryGetLocalRotation(IReadOnlyBone bone, out Quaternion rotation);
+  bool TryGetLocalScale(IReadOnlyBone bone, out Vector3 scale);
+}
---
+++ FinModelUtility/Fin/Fin/src/model/skeleton/RootBoneTransformView.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+namespace fin.model.skeleton;
+
+public class RootBoneTransformView : IBoneTransformView {
+  public bool TryGetWorldTranslation(IReadOnlyBone bone, out Vector3 position) {
+    position = default;
+    return false;
+  }
+
+  public bool TryGetWorldRotation(IReadOnlyBone bone, out Quaternion rotation) {
+    rotation = default;
+    return false;
+  }
+
+  public bool TryGetWorldScale(IReadOnlyBone bone, out Vector3 scale) {
+    scale = default;
+    return false;
+  }
+
+  public bool TryGetLocalTranslation(IReadOnlyBone bone, out Vector3 position) {
+    position = bone.LocalTransform.Translation;
+    return true;
+  }
+
+  public bool TryGetLocalRotation(IReadOnlyBone bone, out Quaternion rotation) {
+    rotation = bone.LocalTransform.Rotation ?? Quaternion.Identity;
+    return true;
+  }
+
+  public bool TryGetLocalScale(IReadOnlyBone bone, out Vector3 scale) {
+    scale = bone.LocalTransform.Scale ?? Vector3.One;
+    return true;
+  }
+}
