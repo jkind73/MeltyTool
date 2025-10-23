8571f83cfbe5a2172b29c98a0e8a5fa021f8766a
MeltyPlayer <meltyplayer@gmail.com>
2025-10-23 13:33:53 -0500

    Started setting up types for GLSL functions, which will simplify how GLSL shaders are programmatically generated.

---
+++ FinModelUtility/Fin/Fin/src/shaders/glsl/functions/FunctionsInterfaces.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace fin.shaders.glsl.functions;
+
+public enum GlslUniform {
+  CAMERA_POSITION,
+  SHININESS,
+}
+
+public enum GlslVarying {
+  VERTEX_NORMAL,
+  VERTEX_POSITION,
+}
+
+public interface IGlslFunction {
+  string Name { get; }
+  IEnumerable<GlslUniform> UniformDependencies { get; }
+  IEnumerable<GlslVarying> VaryingDependencies { get; }
+  IEnumerable<IGlslFunction> FunctionDependencies { get; }
+  void AppendSourceToBuilder(StringBuilder sb);
+}
