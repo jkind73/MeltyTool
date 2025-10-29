785694a3ff9135ee05b3de6d98b150a33499d678
MeltyPlayer <meltyplayer@gmail.com>
2025-10-28 20:23:05 -0500

    Started setting up Sappy project.

---
+++ FinModelUtility/Libraries/Sappy/Sappy/src/schema/Instrument.cs
@@ -0,0 +1,27 @@
+﻿using fin.math;
+
+using schema.binary;
+using schema.binary.attributes;
+
+namespace sappy.schema;
+
+[BinarySchema]
+public partial class InstrumentData : IBinaryConvertible {
+  private uint word0_;
+  private uint word1_;
+  private uint word2_;
+
+  // TODO: Only for some instruments
+
+  [Skip]
+  public byte Attack => (byte) this.word2_.ExtractFromRight(0, 8);
+
+  [Skip]
+  public byte Decay => (byte) this.word2_.ExtractFromRight(8, 8);
+
+  [Skip]
+  public byte Sustain => (byte) this.word2_.ExtractFromRight(16, 8);
+
+  [Skip]
+  public byte Release => (byte) this.word2_.ExtractFromRight(24, 8);
+}
