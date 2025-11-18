987ed883db207a3be90521c5641e9be6c8d36338
MeltyPlayer <meltyplayer@gmail.com>
2025-11-17 21:36:25 -0600

    Added a gatherer for Pokemon Colosseum, just out of curiosity.

---
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool/src/games/pokemon_coliseum/PokemonColosseumFileBundleGatherer.cs
@@ -0,0 +1,16 @@
+﻿using fin.io;
+using fin.io.bundles;
+using fin.util.progress;
+
+namespace uni.games.pokemon_colosseum;
+
+public sealed class PokemonColosseumFileBundleGatherer
+    : BGameCubeFileBundleGatherer {
+  public override string Name => "pokemon_colosseum";
+
+  protected override void GatherFileBundlesFromHierarchy(
+      IFileBundleOrganizer organizer,
+      IMutablePercentageProgress mutablePercentageProgress,
+      IFileHierarchy fileHierarchy) {
+  }
+}
