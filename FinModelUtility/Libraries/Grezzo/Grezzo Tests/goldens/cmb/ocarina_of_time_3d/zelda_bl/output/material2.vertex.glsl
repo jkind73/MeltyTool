e0df3fd3575cfc5f5270abf6e227706cfd5b6309
MeltyPlayer <meltyplayer@gmail.com>
2026-01-14 21:31:45 -0600

    Added a Grezzo test with 4444 textures to prepare for reading these more directly.

---
+++ FinModelUtility/Libraries/Grezzo/Grezzo Tests/goldens/cmb/ocarina_of_time_3d/zelda_bl/output/material2.vertex.glsl
@@ -0,0 +1,37 @@
+#version 460
+
+layout (std140, binding = 1) uniform Matrices {
+  mat4 modelMatrix;
+  mat4 viewMatrix;
+  mat4 projectionMatrix;
+  
+  mat4 boneMatrices[4];  
+};
+
+uniform vec3 cameraPosition;
+
+layout(location = 0) in vec3 in_Position;
+layout(location = 1) in vec3 in_Normal;
+layout(location = 2) in int in_BoneIds;
+layout(location = 3) in float in_BoneWeights;
+layout(location = 4) in vec2 in_Uv0;
+
+out vec3 vertexPosition;
+out vec3 vertexNormal;
+out vec2 uv0;
+
+void main() {
+  mat4 mvMatrix = viewMatrix * modelMatrix;
+  mat4 mvpMatrix = projectionMatrix * mvMatrix;
+  mat4 mergedBoneMatrix = boneMatrices[in_BoneIds] * in_BoneWeights;
+
+
+  mat4 vertexModelMatrix = modelMatrix * mergedBoneMatrix;
+  mat4 projectionVertexModelMatrix = mvpMatrix * mergedBoneMatrix;
+
+  gl_Position = projectionVertexModelMatrix * vec4(in_Position, 1);
+
+  vertexPosition = vec3(vertexModelMatrix * vec4(in_Position, 1));
+  vertexNormal = normalize(vertexModelMatrix * vec4(in_Normal, 0)).xyz;
+  uv0 = in_Uv0;
+}
