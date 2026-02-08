6721a37c7067c4a8e234b6bd828a188eed098f3c
MeltyPlayer <meltyplayer@gmail.com>
2026-02-08 14:15:24 -0600

    Added a helper method for removing the top-left color in an image.

---
+++ FinModelUtility/Fin/Fin/src/image/ImageExtensions.cs
@@ -0,0 +1,73 @@
+﻿using fin.image.formats;
+
+using SixLabors.ImageSharp.PixelFormats;
+
+using Color = System.Drawing.Color;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace fin.image;
+
+public static class ImageExtensions {
+  public static IImage RemoveTopLeftBackgroundColor(this IReadOnlyImage src) {
+    Color topLeftBackgroundColor = default;
+    src.Access(getHandler => {
+      getHandler(0, 0, out var r, out var g, out var b, out _);
+      topLeftBackgroundColor = Color.FromArgb(r, g, b);
+    });
+    return src.RemoveBackgroundColor(topLeftBackgroundColor);
+  }
+
+  public static unsafe IImage RemoveBackgroundColor(this IReadOnlyImage src,
+                                                    Color color) {
+    var width = src.Width;
+    var height = src.Height;
+
+    var textureImageWithAlpha = new Rgba32Image(src.PixelFormat, width, height);
+    using var alphaLock = textureImageWithAlpha.UnsafeLock();
+    var alphaScan0 = alphaLock.pixelScan0;
+
+    src.Access(getHandler => {
+      for (var y = 0; y < height; ++y) {
+        for (var x = 0; x < width; ++x) {
+          getHandler(x,
+                     y,
+                     out var r,
+                     out var g,
+                     out var b,
+                     out var a);
+
+          if (r == color.R && g == color.G && b == color.B) {
+            a = 0;
+          }
+
+          alphaScan0[y * width + x] = new Rgba32(r, g, b, a);
+        }
+      }
+    });
+
+    return textureImageWithAlpha;
+  }
+
+  public static IImage SubImage(this IReadOnlyImage src, Rectangle region) {
+    var dst = new Rgba32Image(src.PixelFormat, region.Width, region.Height);
+
+    var dstLock = dst.Lock();
+
+    src.Access(getHandler => {
+      for (var dstY = 0; dstY < dst.Height; ++dstY) {
+        var srcY = region.Y + dstY;
+
+        for (var dstX = 0; dstX < dst.Width; ++dstX) {
+          var srcX = region.X + dstX;
+
+          getHandler(srcX, srcY, out var r, out var g, out var b, out var a);
+
+          var dstI = dstY * dst.Width + dstX;
+          dstLock.Pixels[dstI] = new Rgba32(r, g, b, a);
+        }
+      }
+    });
+
+    return dst;
+  }
+}
--- FinModelUtility/Fin/Fin/src/image/Images.cs
+++ FinModelUtility/Fin/Fin/src/image/Images.cs
@@ -51,60 +51,6 @@ public static class FinImage {
     return await FromStreamAsync(stream);
   }
 
-  public static unsafe IImage RemoveBackgroundColor(this IReadOnlyImage src,
-                                                    Color color) {
-    var width = src.Width;
-    var height = src.Height;
-
-    var textureImageWithAlpha = new Rgba32Image(src.PixelFormat, width, height);
-    using var alphaLock = textureImageWithAlpha.UnsafeLock();
-    var alphaScan0 = alphaLock.pixelScan0;
-
-    src.Access(getHandler => {
-      for (var y = 0; y < height; ++y) {
-        for (var x = 0; x < width; ++x) {
-          getHandler(x,
-                     y,
-                     out var r,
-                     out var g,
-                     out var b,
-                     out var a);
-
-          if (r == color.R && g == color.G && b == color.B) {
-            a = 0;
-          }
-
-          alphaScan0[y * width + x] = new Rgba32(r, g, b, a);
-        }
-      }
-    });
-
-    return textureImageWithAlpha;
-  }
-
-  public static IImage SubImage(this IReadOnlyImage src, Rectangle region) {
-    var dst = new Rgba32Image(src.PixelFormat, region.Width, region.Height);
-
-    var dstLock = dst.Lock();
-
-    src.Access(getHandler => {
-      for (var dstY = 0; dstY < dst.Height; ++dstY) {
-        var srcY = region.Y + dstY;
-
-        for (var dstX = 0; dstX < dst.Width; ++dstX) {
-          var srcX = region.X + dstX;
-
-          getHandler(srcX, srcY, out var r, out var g, out var b, out var a);
-
-          var dstI = dstY * dst.Width + dstX;
-          dstLock.Pixels[dstI] = new Rgba32(r, g, b, a);
-        }
-      }
-    });
-
-    return dst;
-  }
-
   public static Configuration ImageSharpConfig { get; }
 
   static FinImage() {
@@ -121,19 +67,28 @@ public static class FinImage {
   public static async Task<IImage> FromStreamAsync(Stream stream) {
     var decoderOptions =
         new DecoderOptions { Configuration = ImageSharpConfig };
-    var image = Image.Load(decoderOptions, stream);
+    var image = await Image.LoadAsync(decoderOptions, stream);
+    return FromImageSharpImage(image);
+  }
 
+  public static IImage FromImageSharpImage(Image image) {
     var pixelFormat = image.GetType().GenericTypeArguments[0];
     if (pixelFormat == typeof(Rgba32)) {
       return new Rgba32Image(PixelFormat.RGBA8888,
                              Asserts.CastNonnull(image as Image<Rgba32>));
-    } else if (pixelFormat == typeof(Rgb24)) {
+    }
+
+    if (pixelFormat == typeof(Rgb24)) {
       return new Rgb24Image(PixelFormat.RGB888,
                             Asserts.CastNonnull(image as Image<Rgb24>));
-    } else if (pixelFormat == typeof(L8)) {
+    }
+
+    if (pixelFormat == typeof(L8)) {
       return new L8Image(PixelFormat.L8,
                          Asserts.CastNonnull(image as Image<L8>));
-    } else if (pixelFormat == typeof(La16)) {
+    }
+
+    if (pixelFormat == typeof(La16)) {
       return new La16Image(PixelFormat.LA88,
                            Asserts.CastNonnull(image as Image<La16>));
     }
