35068729007f392c5dcbdf75262cc69442600595
MeltyPlayer <meltyplayer@gmail.com>
2026-02-12 23:48:14 -0600

    Fixed AL13PixelReader by matching what NoClip does.

--- FinModelUtility/Fin/Fin/src/image/io/pixel/Al13PixelReader.cs
+++ FinModelUtility/Fin/Fin/src/image/io/pixel/Al13PixelReader.cs
@@ -26,10 +26,16 @@ public sealed class Al13PixelReader : IPixelReader<La16> {
   }
 
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  private static La16 FromNibble_(byte nibble) {
-    return new La16((byte) BitLogic.ExtractFromRight(nibble, 1, 3),
-                    (byte) (nibble.GetBit(0) ? 255 : 0));
-  }
+  private static La16 FromNibble_(byte nibble)
+    => new(Expand3to8_((nibble >> 1) & 0x7),
+           (byte) (nibble.GetBit(0) ? 255 : 0));
+
+  /// <summary>
+  ///   Shamelessly stolen from:
+  ///   https://github.com/magcius/noclip.website/blob/1d5ee0e1c4fd4f447456f0019b6d64dff2fc9bd1/src/Common/N64/Image.ts#L56C1-L58C2
+  /// </summary>
+  private static byte Expand3to8_(int n)
+    => (byte) ((n << (8 - 3)) | (n << (8 - 6)) | (n >>> (9 - 8)));
 
   public int PixelsPerRead => 2;
   public int BitsPerPixel => 4;
