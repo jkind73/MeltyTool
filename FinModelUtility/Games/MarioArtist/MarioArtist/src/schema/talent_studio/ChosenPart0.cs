2e4e03f322a2ae1aea3707746843a26c9cf70536
MeltyPlayer <meltyplayer@gmail.com>
2026-02-15 03:52:39 -0600

    Documented ChosenPart0 a bit more.

--- FinModelUtility/Games/MarioArtist/MarioArtist/src/schema/talent_studio/ChosenPart0.cs
+++ FinModelUtility/Games/MarioArtist/MarioArtist/src/schema/talent_studio/ChosenPart0.cs
@@ -30,16 +30,14 @@ public sealed partial class ChosenPart0 : IBinaryDeserializable {
   public ChosenColor ChosenColor1 { get; } = new();
 
   public uint[] UnkForPattern0 { get; } = new uint[2];
-
   public uint Pattern0SegmentedAddress { get; set; }
   public PatternMaterialType Pattern0MaterialType { get; set; }
+  
   public uint[] Unk2 { get; } = new uint[2];
-
   public uint Pattern1SegmentedAddress { get; set; }
   public PatternMaterialType Pattern1MaterialType { get; set; }
+
   public uint[] Unk3 { get; } = new uint[2];
-
-  public uint UnkSegmentedAddress2 { get; set; }
-
-  public uint Unk4 { get; set; }
+  public uint MarkSegmentedAddress { get; set; }
+  public PatternMaterialType MarkMaterialType { get; set; }
 }
