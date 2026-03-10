4a57c95b6dd945af8d5a269a8dd24ad8d2203fd6
MeltyPlayer <mastergamepro@hotmail.com>
2026-03-09 23:00:39 -0500

    Create NddemoFileBundleGatherer.cs

---
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool/src/games/nddemo/NddemoFileBundleGatherer.cs
@@ -0,0 +1,20 @@
+﻿using fin.io;
+using fin.io.bundles;
+using fin.util.progress;
+
+using uni.platforms.gcn;
+
+namespace uni.games.nddemo;
+
+public sealed class NddemoFileBundleGatherer : BGameCubeFileBundleGatherer {
+  public override string Name => "nddemo";
+
+  public override GcnFileHierarchyExtractor.Options Options
+    => GcnFileHierarchyExtractor.Options.Standard();
+
+  protected override void GatherFileBundlesFromHierarchy(
+      IFileBundleOrganizer organizer,
+      IMutablePercentageProgress mutablePercentageProgress,
+      IFileHierarchy fileHierarchy) {
+  }
+}
