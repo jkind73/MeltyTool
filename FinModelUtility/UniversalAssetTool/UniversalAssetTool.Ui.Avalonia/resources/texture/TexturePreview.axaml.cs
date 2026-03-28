10319d84d3587ef2405f9839ca3f7078ab39ee69
MeltyPlayer <meltyplayer@gmail.com>
2026-03-28 14:14:09 -0500

    Fixed how images are copied to the clipboard in Avalonia.

--- FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.Avalonia/resources/texture/TexturePreview.axaml.cs
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.Avalonia/resources/texture/TexturePreview.axaml.cs
@@ -1,9 +1,11 @@
 using System.Drawing;
 using System.IO;
+using System.Threading.Tasks;
 
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Input.Platform;
 using Avalonia.Interactivity;
 using Avalonia.Threading;
 
@@ -40,8 +42,9 @@ public class TexturePreviewViewModel : BViewModel {
     set {
       this.RaiseAndSetIfChanged(ref field, value);
       this.Image = null;
-      Dispatcher.UIThread.Invoke(() => this.Image
-                   = value?.AsMergedMipmapAvaloniaImage() ?? missingImage_);
+      this.ImageAsync = Dispatcher.UIThread.InvokeAsync(
+          () => this.Image = value?.AsMergedMipmapAvaloniaImage() ??
+                             missingImage_);
     }
   }
 
@@ -49,6 +52,8 @@ public class TexturePreviewViewModel : BViewModel {
     get;
     set => this.RaiseAndSetIfChanged(ref field, value);
   }
+
+  public DispatcherOperation<Bitmap>? ImageAsync { get; set; }
 
   public Thickness ImageMargin {
     get;
@@ -70,59 +75,8 @@ public partial class TexturePreview : UserControl {
       return;
     }
 
-    var texture = this.ViewModel_.Texture;
-    if (texture == null) {
-      return;
-    }
-
-    using var ms = new MemoryStream();
-    var firstImage = texture.Image;
-    var mipmapImages = texture.MipmapImages;
-
-    if (mipmapImages.Length == 1) {
-      firstImage.ExportToStream(ms, LocalImageFormat.PNG);
-    } else {
-      using var mergedMipmapImage = this.GetMergedMipmapImage_(texture);
-      mergedMipmapImage.ExportToStream(ms, LocalImageFormat.PNG);
-    }
-
-    var dataObject = new DataObject();
-    var formatName = "image/png";
-    dataObject.Set(formatName, ms.ToArray());
-
-    await clipboard.SetDataObjectAsync(dataObject);
-  }
-
-  private unsafe Rgba32Image GetMergedMipmapImage_(IReadOnlyTexture texture) {
-    var firstImage = texture.Image;
-    var dst = new Rgba32Image(2 * firstImage.Width, firstImage.Height);
-    using var dstLock = dst.UnsafeLock();
-    var pixels = dstLock.pixelScan0;
-
-    var mipmapImages = texture.MipmapImages;
-    var baseDstY = 0;
-    for (var i = 0; i < mipmapImages.Length; i++) {
-      var baseDstX = i == 0 ? 0 : firstImage.Width;
-
-      var mipmapImage = mipmapImages[i];
-      mipmapImage.Access(get => {
-        for (var srcY = 0; srcY < mipmapImage.Height; ++srcY) {
-          for (var srcX = 0; srcX < mipmapImage.Width; ++srcX) {
-            get(srcX, srcY, out var r, out var g, out var b, out var a);
-
-            var dstX = baseDstX + srcX;
-            var dstY = baseDstY + srcY;
-
-            pixels[dstY * dst.Width + dstX] = new Rgba32(r, g, b, a);
-          }
-        }
-      });
-
-      if (i >= 1) {
-        baseDstY += mipmapImage.Height;
-      }
-    }
-
-    return dst;
+    var bitmapTask = this.ViewModel_.ImageAsync?.GetTask();
+    var bitmap = bitmapTask != null ? await bitmapTask : null;
+    await clipboard.SetBitmapAsync(bitmap);
   }
 }
