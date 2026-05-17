59184e5c088faeba0f6a59c8c2a2f087e868bef3
MeltyPlayer <meltyplayer@gmail.com>
2026-05-16 23:58:54 -0500

    Set up a way for rendering model tool stuff directly in OpenTK.

---
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.OpenTk/SceneViewerWindow.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+
+using fin.ui;
+using fin.ui.rendering.gl;
+using fin.util.time;
+
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace UniversalAssetTool.Ui.Gl;
+
+public class SceneViewerWindow : GameWindow {
+  private readonly SceneViewerGl viewerImpl_ = new();
+  private readonly TimedCallback fpsTimer_;
+
+  public SceneViewerWindow(GameWindowSettings gameWindowSettings,
+                           NativeWindowSettings nativeWindowSettings) : base(
+      gameWindowSettings,
+      nativeWindowSettings) {
+    this.viewerImpl_.ShowGrid = true;
+    this.fpsTimer_ = new(() => this.Title = FrameTime.FpsString, 1);
+  }
+
+  protected override void OnLoad() {
+    base.OnLoad();
+
+    GlUtil.SwitchContext(this.Context);
+    GlUtil.InitGl();
+  }
+
+  protected override void OnUpdateFrame(FrameEventArgs e) {
+    base.OnUpdateFrame(e);
+  }
+
+  protected override void OnRenderFrame(FrameEventArgs e) {
+    base.OnRenderFrame(e);
+
+    GlUtil.SwitchContext(this.Context);
+
+    this.viewerImpl_.Width = this.Size.X;
+    this.viewerImpl_.Height = this.Size.Y;
+
+    this.viewerImpl_.GlobalScale = UiConstants.GLOBAL_SCALE;
+    this.viewerImpl_.NearPlane = UiConstants.NEAR_PLANE;
+    this.viewerImpl_.FarPlane = UiConstants.FAR_PLANE;
+
+    this.viewerImpl_.Render();
+
+    this.SwapBuffers();
+  }
+}
