1a43e62c704132f16950a5e12658a68f5d26fa22
MeltyPlayer <meltyplayer@gmail.com>
2026-06-06 14:13:29 -0500

    Started copying over stuff from Noclip for Beetle Adventure Racing.

---
+++ FinModelUtility/Games/BeetleAdventureRacing/BeetleAdventureRacing/src/schema/Uvts.cs
@@ -0,0 +1,36 @@
+﻿using schema.binary;
+using schema.binary.attributes;
+
+namespace bar.schema;
+
+public enum UvtsAnimationMode : byte {
+  PlayOnce = 0,
+  Loop = 1,
+  Bounce = 2
+}
+
+/// <summary>
+///   Texture Sequence
+/// 
+///   Shamelessly stolen from:
+///   https://github.com/magcius/noclip.website/blob/main/src/BeetleAdventureRacing/ParsedFiles/UVTS.ts
+/// </summary>
+[BinarySchema]
+public sealed partial class Uvts : IBinaryConvertible {
+  [IntegerFormat(SchemaIntegerType.BYTE)]
+  public bool LoadUvtxesImmediately { get; set; }
+
+  [SequenceLengthSource(SchemaIntegerType.BYTE)]
+  public UvtsFrame[] Frames { get; private set; }
+
+  public UvtsAnimationMode AnimationMode { get; set; }
+
+  [IntegerFormat(SchemaIntegerType.BYTE)]
+  public bool PlayAnimationInReverse { get; set; }
+  public float UnitsPerSecond { get; set; }
+}
+
+public sealed partial class UvtsFrame : IBinaryConvertible {
+  public ushort UvtxIndex { get; set; }
+  public float FrameLengthUnits { get; set; }
+}
---
+++ FinModelUtility/Games/BeetleAdventureRacing/BeetleAdventureRacing/src/schema/Uvtx.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+using schema.binary;
+using schema.binary.attributes;
+
+namespace bar.schema;
+
+/// <summary>
+///   Texture
+/// 
+///   Shamelessly stolen from:
+///   https://github.com/magcius/noclip.website/blob/main/src/BeetleAdventureRacing/ParsedFiles/UVTX.ts
+/// </summary>
+[BinarySchema]
+public sealed partial class Uvtx : IBinaryDeserializable {
+  public ushort TexelDataSize { get; set; }
+  public ushort DlCommandCount { get; set; }
+
+  public Vector2 TexScrollAnim0Velocity { get; }
+  public Vector2 TexScrollAnim1Velocity { get; }
+  public Vector2 TexScrollAnim1Offset { get; }
+
+  [RSequenceLengthSource(nameof(TexelDataSize))]
+  public byte[] TexelData { get; private set; }
+
+  [RSequenceLengthSource(nameof(DlCommandCount))]
+  public byte[] DlCommands { get; private set; }
+}
