d1b3646ca939a9a1ca1a93f455e65a80e1069d2c
MeltyPlayer <meltyplayer@gmail.com>
2026-07-01 21:45:27 -0500

    Set up golden tests for The Labyrinth Plus Edition.

---
+++ FinModelUtility/Games/TheLabyrinthPlusEdition/TheLabyrinthPlusEdition Tests/ScbModelGoldenTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+using fin.io;
+using fin.testing.model;
+using fin.testing;
+
+using tlpe.api;
+
+
+namespace tlpe;
+
+public sealed class ScbModelGoldenTests
+    : BModelGoldenTests<ScbModelFileBundle, ScbModelImporter> {
+  [Test]
+  [TestCaseSource(nameof(GetGoldenDirectories_))]
+  public async Task TestExportsGoldenAsExpected(
+      IFileHierarchyDirectory goldenDirectory)
+    => await this.AssertGolden(goldenDirectory);
+
+  public override ScbModelFileBundle GetFileBundleFromDirectory(
+      IFileHierarchyDirectory directory)
+    => new(directory.GetFilesWithFileType(".scb").Single(),
+           directory.AssertGetExistingFile("Balls.gam"),
+           directory);
+
+  private static IFileHierarchyDirectory[] GetGoldenDirectories_()
+    => GoldenAssert
+       .GetGoldenDirectories(
+           GoldenAssert
+               .GetRootGoldensDirectory(Assembly.GetExecutingAssembly()))
+       .ToArray();
+}
